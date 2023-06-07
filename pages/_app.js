dfeadeb07111efbb20c3aa097a7636979e735c92
Glenn Inizan <glenn@tractr.net>
2023-06-07 14:33:04 -0600

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,24 @@
-import '../styles/globals.css';
-import { Toaster } from 'react-hot-toast';
-import { UserProvider } from '../UserProvider';
+import "../styles/globals.css";
+import { Toaster } from "react-hot-toast";
+import { UserProvider } from "../UserProvider";
+import { NhostProvider, NhostClient } from "@nhost/nextjs";
+import { NhostApolloProvider } from "@nhost/react-apollo";
+
+const nhost = new NhostClient({
+  subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN || "",
+  region: process.env.NEXT_PUBLIC_NHOST_REGION || "",
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-      <Toaster />
-    </UserProvider>
+    <NhostProvider nhost={nhost} initial={pageProps.nhostSession}>
+      <NhostApolloProvider nhost={nhost}>
+        <UserProvider>
+          <Component {...pageProps} />
+          <Toaster />
+        </UserProvider>
+      </NhostApolloProvider>
+    </NhostProvider>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import styles from '../styles/pages/Home.module.css';
+import styles from "../styles/pages/Home.module.css";
 
-import { useUserContext } from '../UserProvider';
-import Head from 'next/head';
-import Layout from '../components/Layout';
+import { useUserContext } from "../UserProvider";
+import Head from "next/head";
+import Layout from "../components/Layout";
+import withAuth from "../withAuth";
 
 const Home = () => {
   const { user } = useUserContext();
@@ -16,19 +17,19 @@ const Home = () => {
       <div>
         <h2 className={styles.title}>Dashboard</h2>
 
-        <p className={styles['welcome-text']}>
-          Welcome, {user?.metadata?.firstName || 'stranger'}{' '}
+        <p className={styles["welcome-text"]}>
+          Welcome, {user?.metadata?.firstName || "stranger"}{" "}
           <span role="img" alt="hello">
             👋
           </span>
         </p>
 
-        <p className={styles['info-text']}>
+        <p className={styles["info-text"]}>
           Edit the <code>pages/index.js</code> file to populate this page.
         </p>
       </div>
     </Layout>
   );
-}
+};
 
-export default Home;
+export default withAuth(Home);
